339de94234d175ae908cd066ff04c6e093ebf4b8
MohammadTaha Rahimi <mohammadtahar2007@gmail.com>
2025-12-27 14:33:56 +0330

    Add NavBar of site
    this is just test of navbar
    it will be on the component as soon as

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
+      {/* NavBar */}
+      <div class="bottom-row">
+        <div class="container t-flex">
+          <a href="./main-page.php" class="logo t-flex nav">
+            <img src="./public/inli.png" alt="" />
+          </a>
+
+          <ul class="t-flex nav-bar">
+            <li class="nav">
+              <a href="./about.php">درباره </a>
+            </li>
+
+            <li class="nav">
+              <a href="./contact-us.php">تماس</a>
+            </li>
+
+            <li class="nav">
+              <a href="./products.php">محصولات</a>
+            </li>
+
+            <li class="nav">
+              <a href="./services.php">خدمات</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* End NavBar */}
+
+      {/* ******************************* */}
     </>
   )
 }
